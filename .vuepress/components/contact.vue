195e294152419bac9fabcc4843076b26e423447f
John Raub <raublekick@gmail.com>
2019-08-25 12:11:33 -0700

    Updating contact component

--- .vuepress/components/contact.vue
+++ .vuepress/components/contact.vue
@@ -1,7 +1,12 @@
 <template>
+  <div>
     <div>
       Questions? Email <a href="mailto:garfield.plants@gmail.com">garfield.plants@gmail.com</a> or call <a href="tel:602-759-0156">(602) 759-0156</a>
     </div>
+    <div>
+      See our <a href='/faq.html'>Frequently Asked Questions</a> page for more information.
+    </div>
+  </div>
 </template>
 
 <style lang="stylus" scoped>
